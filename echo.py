ade4a59c287dbaf39d0199503e5cdde82f82deae
vb1152 <vasya.bilich@gmail.com>
2020-04-03 23:08:30 +0300

    config2

--- echo.py
+++ echo.py
@@ -16,7 +16,7 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 
 logger = logging.getLogger(__name__)
 
-TOKEN = bot_token
+TOKEN = config.bot_token
 
 # Define a few command handlers. These usually take the two arguments update and
 # context. Error handlers also receive the raised TelegramError object in error.
